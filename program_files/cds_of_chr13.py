f484847b04540fb611dbd8f70f536690cdd5732c
WatchmanGu <nightwatchgkj@gmail.com>
2018-04-13 11:09:38 +0800

    chromosome_gene_anotation

---
+++ program_files/cds_of_chr13.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+def sequence_reverse(string):
+    """
+    A function to reverse a sequence.
+    e.g. ATCG --> GCTA.
+    Remind: '+', positive, 5'->3';'-', negative, 3'->5'.
+    """
+    return string[::-1]
--- program_files/chromosome_GO.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-a function to reverse the sequence(e.g. - sequence --> + sequence)
-'+', positive, or 5'->3'; '-', negative, or 3'->5'.
-"""
-def sequence_reverse(string):
-    return string[::-1]
